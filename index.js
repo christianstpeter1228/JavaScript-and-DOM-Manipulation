1a7ec3581eec6ba3bfeb2c963cedfa17d2e32f23
christianstpeter1228 <43319861+christianstpeter1228@users.noreply.github.com>
2019-02-05 19:42:28 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+let $tbody = document.querySelector("tbody");
+let $dateInput = document.querySelector("#date");
+let $cityInput = document.querySelector("#city");
+let $stateInput = document.querySelector("#state");
+let $countryInput = document.querySelector("#country");
+let $shapeInput = document.querySelector("#shape");
+let $searchButton = document.querySelector("#search");
+
+var tableData = data;
+
+function renderTable() {
+    $tbody.innerHTML = "";
+    for (let i = 0; i < tableData.length; i++) {
+        let info = tableData[i];
+        let fields = Object.keys(info);
+        let $row = $tbody.insertRow(i);
+        for (let j = 0; j < fields.length; j++) {
+            let field = fields[j];
+            let $cell = $row.insertCell(j);
+            $cell.innerText = info[field];
+        }
+    }
+}
+
+function handleSearchClick() {
+
+    tableData = dataSet.filter(function(ufoSighting) {
+        let searchDate = ufoSighting.datetime;
+        let searchCity = ufoSighting.city.toLowerCase();
+        let searchState = ufoSighting.state.toLowerCase();
+        let searchCountry = ufoSighting.country.toLowerCase();
+        let searchShape = ufoSighting.shape.toLowerCase();
+
+        if (
+            (searchDate === filterDate || filterDate === "") &&
+            (searchCity === filterCity || filterCity === "") &&
+            (searchState === filterState || filterState === "") &&
+            (searchCountry === filterCountry || filterCountry === "") &&
+            (searchShape === filterShape || filterShape === "")
+        ) {
+            return true;
+        }
+        return false;
+    });
+    renderTable();
+
+    $dateInput.value = "";
+    $cityInput.value = "";
+    $stateInput.value = "";
+    $countryInput.value = "";
+    $shapeInput.value = "";
+}
+
+renderTable();
